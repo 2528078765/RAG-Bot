060edb34fb9f8c09edfd54a435e12d337709d011
2528078765 <2528078765@qq.com>
2026-06-30 17:17:26 +0800

    首次上传

---
+++ RAG-Bot.py
@@ -0,0 +1,55 @@
+import os
+os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
+
+from pathlib import Path
+from pypdf import PdfReader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+
+pdf_dir = Path("凡人修仙传")
+documents = []
+
+for pdf_path in sorted(pdf_dir.glob("*.pdf")):
+    reader = PdfReader(pdf_path)
+    full_text = ""
+    for page in reader.pages:
+        text = page.extract_text()
+        if text:
+            full_text += text + "\n"
+    doc = Document(
+        page_content=full_text,
+        metadata={"source":pdf_path.name}
+    )
+    documents.append(doc)
+
+spliter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 50
+)
+
+chunks = spliter.split_documents(documents)
+
+embedding = HuggingFaceEmbeddings(
+    model_name="all-MiniLM-L6-v2",
+    model_kwargs={"device": "cpu"},
+)
+
+vectorsotre = Chroma.from_documents(
+    documents=chunks,
+    embedding=embedding,
+    collection_name="fanren_novel",
+    persist_directory="./chroma_db",
+)
+
+queries = [
+    "谁是主角？",
+    "主角叫什么名字？",
+    "主角家住哪里？"
+]
+
+for q in queries:
+    results = vectorsotre.similarity_search(q,k=2)
+    for j,doc in enumerate(results):
+        print(f"  [{j+1}] {doc.page_content[:100]}...")
